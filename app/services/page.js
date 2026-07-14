ba60168f4abd8034949efd4c2bdf9d0de545bcf6
mohdamircode <amir113918amir@gmail.com>
2026-07-13 23:49:13 -0700

    intro,routing in nextjs

---
+++ app/services/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Services(){
+    return (
+        <>
+         <h1>Services</h1>
+         <p><Link href="/services/web-dev">Web Development</Link></p>
+         <p><Link href="/">App Development</Link></p>
+         <p><Link href="/">App/Web Design</Link></p>
+         <p><Link href="/">Seo</Link></p>
+         
+        </>
+    )
+}
